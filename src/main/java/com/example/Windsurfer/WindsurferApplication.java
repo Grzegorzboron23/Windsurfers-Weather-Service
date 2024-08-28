a6d6bf6153e458d6bc4a791b3b3f9dabd69f5b05
Grzegorz Boroń <grzegorzboron23@gmail.com>
2024-08-28 08:26:09 +0200

    Initial project setup with basic configuration files

---
+++ src/main/java/com/example/Windsurfer/WindsurferApplication.java
@@ -0,0 +1,13 @@
+package com.example.Windsurfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Windsurfer/WindsurferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Windsurfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
